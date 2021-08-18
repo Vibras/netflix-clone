a8e5159ac7d79864c9d70a8822981d9c192a101c
Ricky <rheredia420@gmail.com>
2021-08-17 19:18:25 -0600

    Added Youtube trailer capability and deployed.

--- src/Row.js
+++ src/Row.js
@@ -1,18 +1,14 @@
-import React, {
-    useState,
-    useEffect
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios.js';
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({
-    title,
-    fetchURL,
-    isLargeRow
-}) {
+function Row({ title, fetchURL, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     // Code which runs based on a specific condition/variable
     useEffect(() => {
@@ -24,25 +20,47 @@ function Row({
         fetchData();
     }, [fetchURL]);
 
-    // console.table(movies);
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "" )
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error) => console.log(error));
+        }
+    };
 
     return ( 
         <div className = "row">
-         <h2 > {title } </h2>
+         <h2 > { title } </h2>
 
         <div className = "row_posters" > 
             { /* row_poster(s) */ } 
             {movies.map(movie => (
-                <img key = {movie.id} 
+                <img 
+                key = {movie.id}
+                onClick={() => handleClick(movie)} 
                 className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
                 src = {`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt = {movie.name}/>
             ))
         }
-
+            {/* container -> posters */ }   
         </div>
-        {/* container -> posters */ }
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
-    )
+    );
 }
 
 export default Row
